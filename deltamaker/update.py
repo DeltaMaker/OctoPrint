746f5587b66197a3d0a35bd2f7ac2bdd4ee6a55a
DeltaMaker <bobh@Bobs-MacBook-Air-2.local>
2014-04-19 09:47:41 -0400

    update script will replace config.yaml
    the first time the firmware is updated, a new config.yaml file must be
    installed

--- deltamaker/update.py
+++ deltamaker/update.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# this script is run at boot to patch config.yaml or load firmware, if necessary
 
 import os
 import sys
@@ -6,9 +7,14 @@ import subprocess
 import serial
 import time
 
+grep = "grep resetsettings.py /home/pi/.octoprint/config.yaml"
+copycfg = "cp -b /home/pi/OctoPrint/deltamaker/config.yaml /home/pi/.octoprint"
 restart = "sudo shutdown -r now"
 command = "avrdude -p m2560 -c stk500v2 -P /dev/ttyUSB0 -U flash:w:/home/pi/Marlin.cpp.hex"
 octoprint = "sudo -u pi /home/pi/bin/octoprint"
+
+if subprocess.call(grep.split()) == 1: #must replace config.yaml file
+    subprocess.call(copycfg.split())
 
 def resetSerial():
     ser = serial.Serial("/dev/ttyUSB0")
